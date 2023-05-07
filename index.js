e64f4e0168ba2529a3890ce5325e8a59eb5bc3f7
cools <sumeetkumar1709@gmail.com>
2023-05-07 15:43:10 +0530

    added atcoder scraper

--- index.js
+++ index.js
@@ -122,8 +122,100 @@ async function codeforces(){
 
 
 
+let atcoderjson={};
 
 
+
+async function atcoder(){
+  let options = {};
+  const url='https://atcoder.jp/contests/';
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+      };
+  }
+
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.setViewport({width: 1080, height: 1024});
+  await delay(4000);
+  bodyHTML = await page.evaluate(() =>  document.documentElement.outerHTML);
+  //console.log(bodyHTML);
+  await browser.close();
+
+  let html=bodyHTML;
+    $ = cheerio.load(html)
+    let i=0;
+    let table
+    $('table',html).each(function(){
+        let temp=$(this).html()
+        if(i===1){
+           table =temp;
+        }
+        i+=1; 
+    });
+
+    $ = cheerio.load(table);
+    let names=[];
+    i=0;
+    var k=0;
+    $('td a',table).each(function(){
+        if(i%2===1){
+          names[k++]=$(this).html();
+        }
+        i++;
+    });
+    //console.log(names);
+    
+    let times=[];
+    i=0;
+    $('td a time',table).each(function(){
+          times[i++]=$(this).html();
+    });
+    //console.log(times);
+
+    let td = [];
+    i=0;
+    $('.text-center',table).each(function(){
+      td[i++]=$(this).html();
+    });
+    
+    i
+    let temp=1;
+    let info=[];
+    let j=0;
+    for(let i=5; i<td.length; i++){
+      if(temp===3){
+        temp=1;
+      }
+      else{
+        info[j++]=td[i];
+        temp++;
+      }
+      
+    }
+    let obj=[];
+    i=0;
+    for(var t=0; t<names.length;t++){
+        let cur=[];
+        cur['contestname']=names[t];
+        cur['starttime']=times[t];
+        cur['duration'] = info[i];
+        i++;
+        cur['ratedfor']=info[i];
+        i++;
+        obj[`${t}`]={...cur};
+    }
+    
+    //console.log(obj);
+    atcoderjson = {...obj};
+    //console.log(atcoderjson);
+}
 
 
 
@@ -142,6 +234,11 @@ app.get('/codechef', async(req, res)=>{
 app.get('/codeforces', async(req, res)=>{
     await codeforces();
     res.send(codeforcesjson);
+});
+
+app.get('/atcoder', async(req, res)=>{
+    await atcoder();
+    res.send(atcoderjson);
 });
 
 app.listen(process.env.PORT || PORT,()=>{console.log(`listening on port ${PORT}`)});
--- temp.js
+++ temp.js
@@ -1,37 +1,125 @@
 const cheerio = require('cheerio');
 const express = require('express');
 const axios = require('axios');
+let chrome = {};
+let puppeteer;
+/**  this condition is necessary for the puppeteer to work on production and development
+ *  with different dependencies required */
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
 
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
 
 
 const app = express();
 const PORT = 3000;
 
-let codeforcesjson={};
-async function codeforces(){
-    const url = 'https://codeforces.com/api/contest.list?';
-    axios
-    .get(url)
-    .then(response => {
-      //console.log(response.data.result[0].id);
-      let temp = response.data.result;
-      var k=0;
-      for(var i=0;i<temp.length;i++){
-        if(temp[i].phase==="BEFORE"){
-            codeforcesjson[k++] = temp[i];
+let atcoderjson={};
+
+
+
+async function atcoder(){
+  let options = {};
+  const url='https://atcoder.jp/contests/';
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+      };
+  }
+
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.setViewport({width: 1080, height: 1024});
+  await delay(4000);
+  bodyHTML = await page.evaluate(() =>  document.documentElement.outerHTML);
+  //console.log(bodyHTML);
+  await browser.close();
+
+  let html=bodyHTML;
+    $ = cheerio.load(html)
+    let i=0;
+    let table
+    $('table',html).each(function(){
+        let temp=$(this).html()
+        if(i===1){
+           table =temp;
         }
+        i+=1; 
+    });
+
+    $ = cheerio.load(table);
+    let names=[];
+    i=0;
+    var k=0;
+    $('td a',table).each(function(){
+        if(i%2===1){
+          names[k++]=$(this).html();
+        }
+        i++;
+    });
+    //console.log(names);
+    
+    let times=[];
+    i=0;
+    $('td a time',table).each(function(){
+          times[i++]=$(this).html();
+    });
+    //console.log(times);
+
+    let td = [];
+    i=0;
+    $('.text-center',table).each(function(){
+      td[i++]=$(this).html();
+    });
+    
+    i
+    let temp=1;
+    let info=[];
+    let j=0;
+    for(let i=5; i<td.length; i++){
+      if(temp===3){
+        temp=1;
       }
-    })
-    .catch(error => {
-      console.log(error);
-    });  
+      else{
+        info[j++]=td[i];
+        temp++;
+      }
+      
+    }
+    let obj=[];
+    i=0;
+    for(var t=0; t<names.length;t++){
+        let cur=[];
+        cur['contestname']=names[t];
+        cur['starttime']=times[t];
+        cur['duration'] = info[i];
+        i++;
+        cur['ratedfor']=info[i];
+        i++;
+        obj[`${t}`]={...cur};
+    }
+    
+    //console.log(obj);
+    atcoderjson = {...obj};
+    //console.log(atcoderjson);
 }
 
 
-
-
-codeforces();
 
 
 app.get('/', (req, res)=>{
@@ -39,8 +127,9 @@ app.get('/', (req, res)=>{
 });
 
 
-app.get('/codeforces', async(req, res)=>{
-    res.send(codeforcesjson);
+app.get('/atcoder', async(req, res)=>{
+    await atcoder();
+    res.send(atcoderjson);
 });
 
 app.listen(process.env.PORT || PORT,()=>{console.log(`listening on port ${PORT}`)});
